f4cc3cf3e2372a9304f2db4427de8af9409f3679
Tomasz Gieorgijewski <github@gieorgijewski.pl>
2020-11-27 11:47:44 +0100

    Update header.cs
    Small fix in file comment

--- templates/default-csharp/header.cs
+++ templates/default-csharp/header.cs
@@ -6,6 +6,6 @@ using System.Collections;
 using System.Collections.Generic;
 
 
-// This is a highly volatile file. Do not edit it. Instead - edit codingame/main.py.
+// This is a highly volatile file. Do not edit it. Instead - edit codingame/main.cs.
 // All of the changes that you will put in here will be overriden by next run of
 // CGMerger.
