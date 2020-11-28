af030d33c9d093687651f896a3c85d59904942b8
Adam Jakubowski <ajakubo1@gmail.com>
2020-11-28 18:59:34 +0100

    Adding a regular file

--- tests/test_file_doesnt_exist.py
+++ tests/test_file_doesnt_exist.py
@@ -216,3 +216,29 @@ class FileDoesntExist(unittest.TestCase):
             ],
             any_order=True,
         )
+
+    @patch("cgmerger.cgmerge.parser")
+    @patch("cgmerger.cgmerge.os.path.isfile")
+    @patch("cgmerger.cgmerge.os.path.isdir")
+    @patch("cgmerger.cgmerge.os.path.getsize")
+    @patch("cgmerger.cgmerge.os.listdir")
+    @patch("cgmerger.cgmerge.open")
+    @patch("cgmerger.cgmerge.chardet.detect")
+    def test_add_file(
+        self, detect, open, listdir, getsize, is_dir, path_exists, parser
+    ):
+        detect.return_value = {"encoding": "utf-8"}
+        getsize.return_value = 1
+        path_exists.return_value = True
+        is_dir.return_value = True
+        listdir.return_value = ["merge_me.py"]
+        self.get_default_setup(parser)
+        main()
+        open.assert_has_calls(
+            [
+                call("codingame.volatile.py", "w"),
+                call("codingame/merge_me.py", "rb"),
+                call("codingame/merge_me.py", "r", encoding="utf-8"),
+            ],
+            any_order=True,
+        )
